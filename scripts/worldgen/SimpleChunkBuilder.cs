6e8b07c54120a6e9a6471fb0df98bee4e81ebfe0
ZentixDev <zentixdev@gmail.com>
2025-02-05 18:15:39 +0100

    Chunk boarder with stone for seeing chunks

--- scripts/worldgen/SimpleChunkBuilder.cs
+++ scripts/worldgen/SimpleChunkBuilder.cs
@@ -17,5 +17,15 @@ public partial class SimpleChunkBuilder : ChunkBuilder
         for (int i = 1; i < Height + 1; i++)
             chunk.FillLayer(i, (ushort)MiddleBlock);
         chunk.FillLayer(Height + 1, (ushort)TopBlock);
+        for (int x = 0; x < Chunk.Size.X; x++)
+        {
+            chunk.SetBlock(new Vector3I(x, Height + 1, 0), (ushort) Blocks.DefaultBlock.Stone);
+            chunk.SetBlock(new Vector3I(x, Height + 1, 15), (ushort) Blocks.DefaultBlock.Stone);
+        }
+        for (int z = 0; z < Chunk.Size.X; z++)
+        {
+            chunk.SetBlock(new Vector3I(0, Height + 1, z), (ushort) Blocks.DefaultBlock.Stone);
+            chunk.SetBlock(new Vector3I(15, Height + 1, z), (ushort) Blocks.DefaultBlock.Stone);
+        }
     }
 }
