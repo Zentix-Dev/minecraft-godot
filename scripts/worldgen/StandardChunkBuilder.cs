ff6b724243867a24f3d094f5fa723202e968d6d0
ZentixDev <zentixdev@gmail.com>
2025-02-06 20:52:22 +0100

    Initial commit

---
+++ scripts/worldgen/StandardChunkBuilder.cs
@@ -0,0 +1,61 @@
+﻿using Godot;
+using Minecraft.scripts.engine;
+
+namespace Minecraft.scripts.worldgen;
+
+[GlobalClass]
+public partial class StandardChunkBuilder : ChunkBuilder
+{
+    [ExportGroup("Terrain Parameters"), Export]
+    public Blocks.DefaultBlock TopBlock;
+    [Export]
+    public Blocks.DefaultBlock MiddleBlock;
+    [Export]
+    public Blocks.DefaultBlock BottomBlock;
+    [Export]
+    public int BottomBlockHeight;
+
+    [Export]
+    public int TerrainHeightMin;
+    [Export]
+    public int TerrainHeightMax;
+    
+    [ExportGroup("Noise Parameters"), Export]
+    public Vector3 Offset;
+
+    [Export] public Vector2 Scale = Vector2.One;
+
+    [Export] public int Octaves = 10;
+    [Export] public float Lacunarity = 2f;
+    [Export] public string Seed = "0";
+    
+    public override void GenerateChunk(Chunk chunk)
+    {
+        var noiseGenerator = new FastNoiseLite()
+        {
+
+            Offset = Offset,
+            FractalLacunarity = Lacunarity,
+            FractalOctaves = Octaves,
+            Seed = Seed.GetHashCode(),
+            NoiseType = FastNoiseLite.NoiseTypeEnum.Simplex
+        };
+        
+        for (int x = 0; x < Chunk.Size.X; x++)
+        for (int z = 0; z < Chunk.Size.Z; z++)
+        {
+            var worldPosition = new Vector2I(x + chunk.ChunkPos.X * Chunk.Size.X, z + chunk.ChunkPos.Y * Chunk.Size.Z);
+            float height = noiseGenerator.GetNoise2D(worldPosition.X + Scale.X, worldPosition.Y * Scale.Y);
+            int terrainHeight = Mathf.RoundToInt(Mathf.Lerp(TerrainHeightMin, TerrainHeightMax, height));
+            for (int y = 0; y < terrainHeight; y++)
+            {
+                if (y == terrainHeight - 1)
+                    chunk.SetBlock(new Vector3I(x, y, z), (ushort)TopBlock);
+                else if (y < BottomBlockHeight)
+                    chunk.SetBlock(new Vector3I(x, y, z), (ushort)BottomBlock);
+                else
+                    chunk.SetBlock(new Vector3I(x, y, z), (ushort)MiddleBlock);
+            }
+        }
+    }
+}
