f329fc5f8f236052ab010c07f3f4ab00c04f7c82
ZentixDev <zentixdev@gmail.com>
2025-02-06 20:52:01 +0100

    Added block placing

--- scripts/game/BlockBreaker.cs
+++ scripts/game/BlockBreaker.cs
@@ -8,17 +8,17 @@ public partial class BlockBreaker : RayCast3D
 {
     public override void _UnhandledInput(InputEvent @event)
     {
-        if (@event is InputEventMouseButton { Pressed: true })
+        if (@event is InputEventMouseButton { Pressed: true, ButtonIndex: MouseButton.Left or MouseButton.Right } mouseButtonEvent)
         {
-            GetViewport().SetInputAsHandled();
-            Vector3 worldPosHit = (GetCollisionPoint() - GetCollisionNormal() * .05f).Round();
+            bool isDestroy = mouseButtonEvent.ButtonIndex == MouseButton.Left;
+            Vector3 worldPosHit = (GetCollisionPoint() - GetCollisionNormal() * (isDestroy ? .05f : -.05f)).Round();
             
             var chunkManager = ChunkManager.Instance;
             Chunk chunk = chunkManager.GetChunkAt(worldPosHit);
             Vector2I chunkPos = chunkManager.GetChunkPosAt(worldPosHit);
             Vector3I posInChunk = chunkManager.GetPosInChunk(worldPosHit);
             GD.Print($"Destroying block: {chunk.GetBlock(posInChunk)} at pos {posInChunk} of chunk {chunkPos}");
-            chunk.SetBlock(posInChunk, (ushort)Blocks.DefaultBlock.Air);
+            chunk.SetBlock(posInChunk, (ushort)(isDestroy ? Blocks.DefaultBlock.Air : Blocks.DefaultBlock.Dirt));
             chunk.UpdateNeighborsImmediate();
             chunk.UpdateMeshImmediate();
         }
