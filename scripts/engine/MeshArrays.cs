470e86f82764423b11a27377a4d0ea180d0cf715
ZentixDev <zentixdev@gmail.com>
2025-02-10 17:27:57 +0100

    Initial commit

---
+++ scripts/engine/MeshArrays.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace Minecraft.scripts.engine;
+
+public class MeshArrays
+{
+    public List<Vector3> Vertices = new();
+    public List<int> Triangles = new();
+    public List<Vector3> Normals = new();
+    public List<Vector2> Uvs = new();
+}
