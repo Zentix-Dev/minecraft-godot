5281d52135927769fb44e1c53611d630105a3984
ZentixDev <zentixdev@gmail.com>
2025-02-18 08:56:03 +0100

    Added Ungenerated Block placing method

--- scripts/worldgen/ChunkBuilder.cs
+++ scripts/worldgen/ChunkBuilder.cs
@@ -7,4 +7,16 @@ namespace Minecraft.scripts.worldgen;
 public abstract partial class ChunkBuilder : Resource
 {
     public abstract void GenerateChunk(Chunk chunk);
+
+    protected void PlaceUngeneratedBlocks(Chunk chunk)
+    {
+        var ungeneratedBlocks = chunk.GetUngeneratedBlocks();
+        if (ungeneratedBlocks == null) return;
+        
+        foreach (var (pos, block) in ungeneratedBlocks)
+        {
+            chunk.SetBlock(pos, block);
+        }
+        chunk.ClearUngeneratedBlocks();
+    }
 }
