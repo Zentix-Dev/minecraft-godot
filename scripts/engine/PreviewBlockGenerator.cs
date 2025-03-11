58dc96c51d01b062eebb6c6e546c47335831ab20
ZentixDev <zentixdev@gmail.com>
2025-03-11 09:23:36 +0100

    Initial commit

---
+++ scripts/engine/PreviewBlockGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Godot;
+
+namespace Minecraft.scripts.engine;
+
+public class PreviewBlockGenerator
+{
+    public ArrayMesh GenerateBlockMesh(Blocks.DefaultBlock block)
+    {
+        var vertices = new List<Vector3>();
+        var triangles = new List<int>();
+        var normals = new List<Vector3>();
+        var uvs = new List<Vector2>();
+
+        var values = Enum.GetValues<MeshUtils.FaceDirection>();
+        foreach (var faceDirection in values)
+            MeshUtils.CreateFace(faceDirection,
+                Vector3.Zero,
+                Blocks.GetTextureIndex((ushort)block,
+                    faceDirection),
+                vertices,
+                triangles,
+                normals,
+                uvs);
+        return MeshUtils.CreateArrayMesh(vertices.ToArray(), triangles.ToArray(), uvs.ToArray(), normals.ToArray());
+    }
+}
