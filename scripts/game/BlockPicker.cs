6c6e758d2be6877e77cb3c0fd4e32ac6910789f8
ZentixDev <zentixdev@gmail.com>
2025-03-18 08:33:14 +0100

    Initial commit

---
+++ scripts/game/BlockPicker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Godot;
+using Minecraft.scripts.engine;
+
+namespace Minecraft.scripts.game;
+
+public partial class BlockPicker : Node3D
+{
+    [Export] private MeshInstance3D _blockMesh;
+
+    private int _selectedBlock = 0;
+
+    public int SelectedBlock
+    {
+        get => _selectedBlock;
+        set
+        {
+            _selectedBlock = value % _blocks.Length;
+            UpdateSelectedBlock();
+        }
+    }
+
+    private Blocks.DefaultBlock[] _blocks = Enum.GetValues<Blocks.DefaultBlock>();
+
+    public override void _Ready()
+    {
+        UpdateSelectedBlock();
+    }
+
+    private void UpdateSelectedBlock()
+    {
+        _blockMesh.Mesh = PreviewBlockGenerator.GenerateBlockMesh(_blocks[_selectedBlock]);
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton {Pressed: true} mouseButtonEvent)
+        {
+            if (mouseButtonEvent.ButtonIndex == MouseButton.WheelDown)
+                SelectedBlock++;
+            else if (mouseButtonEvent.ButtonIndex == MouseButton.WheelUp)
+                SelectedBlock--;
+        }
+    }
+}
